27a5686ec0a41317fcf00574a90b922882619b65
OM PRADHAN <pradhanom149@gmail.com>
2026-06-25 21:08:29 +0530

    Modal Overlay Successfully Render

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const Modal=document.getElementById("modal");
-const openModal = () => { 
-console.log("running");
-Modal.classList.add('hidden');
-};
+const Modal=document.getElementById('modal');
+const Button = document.getElementById('button');
+Button.addEventListener('click', () => {
+Modal.classList.toggle('hidden');
+});
+const close=document.getElementById('Close');
+close.addEventListener('click',() => {
+Modal.classList.toggle('hidden');
+})
